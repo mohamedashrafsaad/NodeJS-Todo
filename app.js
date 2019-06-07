09be03e85fd86a2b553fafb9393062e3670b0eb2
mohamed <mohamed.ashraf.saad22@gmail.com>
2019-06-07 16:18:57 +0200

    4th

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
-var todoController = require('./controllers/todoController');
+// var todoController = require('./controllers/todoController');
+// var express = require('express');
+
+// var app = express();
+// //var port = process.env.PORT || 3000;
+
+// app.listen(process.env.PORT, function () {
+//     console.log(`Example app listening on port !`);
+//    });   //listen to port
+
+
+// // var cors = require('cors');
+
+// // app.options('*', cors())
+// // app.post('*', cors())
+// // app.delete('*', cors())
+
+
+
+
+// todoController(app);     //call controller
+
+
 var express = require('express');
-
+var port = process.env.PORT || 3000;
 var app = express();
-//var port = process.env.PORT || 3000;
-
-app.listen(process.env.PORT, function () {
-    console.log(`Example app listening on port !`);
-   });   //listen to port
-
-
-// var cors = require('cors');
-
-// app.options('*', cors())
-// app.post('*', cors())
-// app.delete('*', cors())
-
-
-
-
-todoController(app);     //call controller
-
-
+app.get('/', function (req, res) {
+ res.send(JSON.stringify({ Hello: 'World'}));
+});
+app.listen(port, function () {
+ console.log(`Example app listening on port !`);
+});
