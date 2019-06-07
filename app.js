1282d7c10367f61ea3a3d95f5e5e1e5a5a6d1697
mohamed <mohamed.ashraf.saad22@gmail.com>
2019-06-07 16:58:01 +0200

    changes

--- app.js
+++ app.js
@@ -1,32 +1,21 @@
-// var todoController = require('./controllers/todoController');
-// var express = require('express');
-
-// var app = express();
-// //var port = process.env.PORT || 3000;
-
-// app.listen(process.env.PORT, function () {
-//     console.log(`Example app listening on port !`);
-//    });   //listen to port
-
-
-// // var cors = require('cors');
-
-// // app.options('*', cors())
-// // app.post('*', cors())
-// // app.delete('*', cors())
-
-
-
-
-// todoController(app);     //call controller
-
-
+var todoController = require('./controllers/todoController');
 var express = require('express');
-var port = process.env.PORT || 3000;
+
 var app = express();
-app.get('/', function (req, res) {
- res.send(JSON.stringify({ Hello: 'World'}));
-});
-app.listen(port, function () {
- console.log(`Example app listening on port !`);
-});
+
+app.set('view engine' , 'ejs');    //template engine
+
+app.use(express.static('./public'));   //static files
+var cors = require('cors');
+
+app.options('*', cors())
+app.post('*', cors())
+app.delete('*', cors())
+
+
+
+
+todoController(app);     //call controller
+
+app.listen(3000);     //listen to port
+console.log("you are listening to port 3000");
--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,51 +1,52 @@
-// var bodyparser = require('body-parser');
-// var mongoose = require('mongoose');
+var bodyparser = require('body-parser');
+var mongoose = require('mongoose');
 
-// mongoose.connect('mongodb+srv://mohamed:1234@cluster0-jtx9u.azure.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true })
+mongoose.connect('mongodb+srv://mohamed:1234@cluster0-jtx9u.azure.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true })
 
-// var todoSchema = new mongoose.Schema({
-//     item: String ,
-//     complete: Boolean
-// });
+var todoSchema = new mongoose.Schema({
+    item: String ,
+    complete: Boolean
+});
 
-// var Todo = mongoose.model('Todo',todoSchema);
-// var urlencodedparser = bodyparser.json();
+var Todo = mongoose.model('Todo',todoSchema);
+var urlencodedparser = bodyparser.json();
 module.exports = function (app) {
-    
     app.get('/', function (req,res) {
+        res.send("ok")
         
-        res.send('ok')
-        // res.header("Access-Control-Allow-Origin", "*");
-        // Todo.find({},function (err,data) {
-        //     if(err) throw err;
-        //     res.send( {todos: data});
+    });
+    app.get('/todo', function (req,res) {
+        res.header("Access-Control-Allow-Origin", "*");
+        Todo.find({},function (err,data) {
+            if(err) throw err;
+            res.send( {todos: data});
             
+        });
         
-        
-    // });
-    // app.post('/', urlencodedparser, function (req,res) {
-    //     res.header("Access-Control-Allow-Origin", "*");
-    //     var newTodo = Todo(req.body).save(function(err){
+    });
+    app.post('/todo', urlencodedparser, function (req,res) {
+        res.header("Access-Control-Allow-Origin", "*");
+        var newTodo = Todo(req.body).save(function(err){
 
-    //         if(err) throw err;
-    //         res.redirect(req.originalUrl)
-    //     });
-    // });
+            if(err) throw err;
+            res.redirect(req.originalUrl)
+        });
+    });
 
 
-    // app.delete('/:item', function (req,res) {
-    //         console.log(req.params.item)
-    //     Todo.findOneAndDelete(({"_id":req.params.item}), (err, todo) => {
-    //         // As always, handle any potential errors:
-    //         if (err) return res.status(500).send(err);
-    //         // We'll create a simple object to send back with a message and the id of the document that was removed
-    //         // You can really do this however you want, though.
-    //         const response = {
-    //             message: "Todo successfully deleted",
-    //             item: req.params.item
-    //         };
-    //         return res.status(200).send(response);
-    //     });
+    app.delete('/:item', function (req,res) {
+            console.log(req.params.item)
+        Todo.findOneAndDelete(({"_id":req.params.item}), (err, todo) => {
+            // As always, handle any potential errors:
+            if (err) return res.status(500).send(err);
+            // We'll create a simple object to send back with a message and the id of the document that was removed
+            // You can really do this however you want, though.
+            const response = {
+                message: "Todo successfully deleted",
+                item: req.params.item
+            };
+            return res.status(200).send(response);
+        });
     });
         // console.log(req.params.item)
         // res.header("Access-Control-Allow-Origin", "*");
